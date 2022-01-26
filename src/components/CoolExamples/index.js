213dff85b040c8bf251adc5bf9b1eea66790fca5
pavtar <avtar@glideinvest.com>
2022-01-26 17:35:12 +0530

    cool examples added

--- src/components/CoolExamples/index.js
+++ src/components/CoolExamples/index.js
@@ -4,7 +4,46 @@ import "./index.css";
 function CoolExamples() {
   return (
     <div className="cool-expample-container">
-      <h3>Cool Examples</h3>
+      <ul className="smoky-text">
+        <li>S</li>
+        <li>M</li>
+        <li>O</li>
+        <li>K</li>
+        <li>Y</li>
+      </ul>
+      <div className="fill-text">
+        <h2>text</h2>
+      </div>
+      <div className="container-social-media">
+        <a href="#" className="icon">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="#" className="icon">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="#" className="icon">
+          <i className="fab fa-google"></i>
+        </a>
+        <a href="#" className="icon">
+          <i className="fab fa-youtube"></i>
+        </a>
+        <a href="#" className="icon">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
+      {/* rotate text border */}
+      <div className="rotate-text-border box ">
+        <span className="rotated"></span>
+        <h1>
+          CSS
+          <br />
+          <span>border</span>
+          <br />
+          hover
+          <br />
+          effect
+        </h1>
+      </div>
     </div>
   );
 }
